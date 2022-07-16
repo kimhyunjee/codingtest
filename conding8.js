078ad625193637e3c75bffbf77fe773f9496d780
kimhyunjee <ysk30430@naver.com>
2022-07-16 17:19:20 +0900

    코딩예제 8 평균구하기

---
+++ conding8.js
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+  <script>
+
+  function solution(arr) {
+    const array = [1, 2, 3, 4];
+    let answer = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+    answer += array[i];
+    }
+        return answer;
+    } 
+
+  </script>
+</body>
+
+</html>
